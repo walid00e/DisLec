8e141a15ed9a820730adf31ae73717114a38db42
Huawei <wakshs4@gmail.com>
2024-05-18 23:55:10 +0200

    added some stuff

--- src/components/Patient/RecordingList.js
+++ src/components/Patient/RecordingList.js
@@ -1,9 +1,16 @@
 import './RecordingList.css';
+import RecordingItem from "./RecordingItem";
 
 function RecordingList(){
     return(
         <div className="recording-list-base rounderCorn dropShadow">
-
+            <p className="recording-list-title">Recording List</p>
+            <RecordingItem specialist="LOREM IPSUM" date="01-04-2024" level="LEVEL 4" exercise="EXERCISE 5" grade="GOOD"/>
+            <RecordingItem specialist="LOREM IPSUM" date="01-04-2024" level="LEVEL 4" exercise="EXERCISE 5" grade="GOOD"/>
+            <RecordingItem specialist="LOREM IPSUM" date="01-04-2024" level="LEVEL 4" exercise="EXERCISE 5" grade="GOOD"/>
+            <RecordingItem specialist="LOREM IPSUM" date="01-04-2024" level="LEVEL 4" exercise="EXERCISE 5" grade="GOOD"/>
+            <RecordingItem specialist="LOREM IPSUM" date="01-04-2024" level="LEVEL 4" exercise="EXERCISE 5" grade="GOOD"/>
+            <RecordingItem specialist="LOREM IPSUM" date="01-04-2024" level="LEVEL 4" exercise="EXERCISE 5" grade="GOOD"/>
         </div>
     );
 }
